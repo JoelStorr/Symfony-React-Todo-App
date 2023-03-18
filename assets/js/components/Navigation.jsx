e8a56f7e794664b2285a300077f26dc6cee83b1c
Joel Storr <joel.storr@ewe.net>
2023-03-18 11:50:13 +0100

    Set Link Styles

--- assets/js/components/Navigation.jsx
+++ assets/js/components/Navigation.jsx
@@ -1,12 +1,13 @@
 //React
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 //MUI Components
 import {
   AppBar,
   Toolbar,
   IconButton,
-  Link,
+
   Box,
   Button,
   Drawer,
@@ -14,12 +15,22 @@ import {
   ListItem,
   ListItemText,
   ListItemIcon,
-
+Typography
 
 } from "@material-ui/core";
 
 //MUI Icons
 import { Menu as MenuIcon, List as ListIcon, Label as LabelIcon } from "@mui/icons-material";
+
+
+
+const LinkStyle={
+    textDecoration:'none',
+    color: 'darkgray'
+}
+
+
+
 
 export default function Navigation() {
 
@@ -34,31 +45,37 @@ export default function Navigation() {
 
 
     const drawerItems = [
-        {text: 'Todo List', icon: <ListIcon />},
-        {text: 'Tags', icon: <LabelIcon />}
+        {text: 'Todo List', icon: <ListIcon />, link: 'todo-list'},
+        {text: 'Tags', icon: <LabelIcon />, link: 'tag-list'}
     ]
  
   return (
-    <AppBar position="fixed" style={{backgroundColor: 'lightgray'}}>
-        <Toolbar>
-           <IconButton edge="start" onClick={toggleDrawer}>
-            <MenuIcon />
-           </IconButton>     
-            <Box style={{flexGrow: 1}}/>
-            <Link href="/" variant='h6' color="textPrimary" underline="none">TodoApp</Link>
-            <Box style={{flexGrow: 1}}/>
-            <Button size="large" > Login </Button>
-        </Toolbar>
-        <Drawer   variant="temporary" onClose={toggleDrawer} open={drawerOpen}>
-            <List style={{width: '200px'}}>
-                {drawerItems.map((prop)=>(
-                    <ListItem button key={prop.text} onClick={toggleDrawer}>
-                        <ListItemIcon>{prop.icon}</ListItemIcon>
-                        <ListItemText>{prop.text}</ListItemText>
-                    </ListItem>
-                ))}
-            </List>
-        </Drawer>
+    <AppBar position="fixed" style={{ backgroundColor: "lightgray" }}>
+      <Toolbar>
+        <IconButton edge="start" onClick={toggleDrawer}>
+          <MenuIcon />
+        </IconButton>
+        <Box style={{ flexGrow: 1 }} />
+        <Link to="/" style={LinkStyle}>
+          <Typography variant="h6" color="textPrimary" underline="none">
+            TodoApp
+          </Typography>
+        </Link>
+        <Box style={{ flexGrow: 1 }} />
+        <Button size="large"> Login </Button>
+      </Toolbar>
+      <Drawer variant="temporary" onClose={toggleDrawer} open={drawerOpen}>
+        <List style={{ width: "200px" }}>
+          {drawerItems.map((prop) => (
+            <Link to={prop.link} key={prop.text} style={LinkStyle}>
+              <ListItem button onClick={toggleDrawer}>
+                <ListItemIcon>{prop.icon}</ListItemIcon>
+                <ListItemText>{prop.text}</ListItemText>
+              </ListItem>
+            </Link>
+          ))}
+        </List>
+      </Drawer>
     </AppBar>
   );
 }
