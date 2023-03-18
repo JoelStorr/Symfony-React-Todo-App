9b8381375379b715e592d0213593fcbaad57aaf5
Joel Storr <joel.storr@ewe.net>
2023-03-18 11:29:23 +0100

    Added React Router

--- assets/js/components/Router.jsx
+++ assets/js/components/Router.jsx
@@ -1,25 +1,54 @@
-import TodoContextProvider from '../context/TodoContext.jsx';
-import TodoTable from './TodoTable';
+
+//React
+import React from 'react';
+
+//Router
+import { createBrowserRouter, RouterProvider, BrowserRouter, Route, Routes, Redirect, Navigate } from 'react-router-dom';
+
+
+// MUI
 import { CssBaseline } from '@mui/material';
+
+// Routes
+import NotFound from './NotFound';
+import TodoContextProvider from '../context/TodoContext';
 import AppSnackBar from './AppSnackBar';
+import Navigation from './Navigation';
+import TodoTable from './TodoTable';
+import TagList from './TagList';
 
-import Navigation from './Navigation.jsx';
 
-import React from 'react'
+
+function TodoList(){
+  return(
+    <TodoContextProvider>
+      <CssBaseline>
+        <TodoTable />
+        <AppSnackBar />
+      </CssBaseline>
+    </TodoContextProvider>
+  )
+}
+
+
+
+
+
 
 export default function Router() {
   return (
-      <>
-
+    <>
+      <BrowserRouter>
         <Navigation />
-            <TodoContextProvider >
-               <CssBaseline>
-                    <TodoTable />
-                    <AppSnackBar />
-               </CssBaseline>      
-            </TodoContextProvider>
-      </>
-       
-    )
+        <div style={{height: '65px'}}></div>
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/todo-list" />} />
+          <Route path="/todo-list" element={<TodoList />} />
+          <Route path="/tag-list" element={<TagList />} />
+          <Route element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
   
---
+++ assets/js/components/TagList.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function TagList() {
+  return (
+    
+    <div>TagList</div>
+  )
+}
